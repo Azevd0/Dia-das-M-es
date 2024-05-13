f56c5a8a15b354ab5532f7a1c25b7ea1a4a5b998
Davyson de Azevedo <158609502+Azevd0@users.noreply.github.com>
2024-05-12 23:25:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('txt').addEventListener('click', function() {
+    var imagem = document.querySelector('.imagem');
+    imagem.style.display = imagem.style.display === 'block' ? 'none' : 'block';
+});
